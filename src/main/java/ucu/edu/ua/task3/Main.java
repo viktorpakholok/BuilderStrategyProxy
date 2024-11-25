7f0b1480d196384303cfdb6f5ca2cabce7523fa3
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-11-25 23:03:39 +0200

    Finalized task3

--- src/main/java/ucu/edu/ua/task3/Main.java
+++ src/main/java/ucu/edu/ua/task3/Main.java
@@ -1,8 +1,8 @@
-// package ucu.edu.ua.task3;
+package ucu.edu.ua.task3;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         MyImage img = new ProxyImage(null);
-//         img.display();
-//     }
-// }
+public class Main {
+    public static void main(String[] args) {
+        MyImage img = new ProxyImage("src/main/resources/proxy.png");
+        img.display();
+    }
+}
--- src/main/java/ucu/edu/ua/task3/ProxyImage.java
+++ src/main/java/ucu/edu/ua/task3/ProxyImage.java
@@ -1,5 +1,21 @@
 package ucu.edu.ua.task3;
 
-public class ProxyImage {
+public class ProxyImage implements MyImage {
+
+    private RealImage realImage;
+    private String filename;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+
+        realImage.display();
+    }
 
 }
